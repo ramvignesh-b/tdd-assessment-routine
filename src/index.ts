e15a1f1ea4b54fb9f5bcd27267213dad787610cf
ramvignesh-b <ramvignesh@incubyte.co>
2025-09-01 12:02:24 +0530

    feat: implement routine check for read

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export const checkRoutine = () => {
+  const currentHour = new Date().getHours();
+  
+  if (currentHour >= 7 && currentHour < 8) {
+    return "Read and study";
+  }
+
   return "Do Exercise";
 }
