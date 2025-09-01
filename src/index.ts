ab4831dabd93054e2251b737f060f1a6998786a2
ramvignesh-b <ramvignesh@incubyte.co>
2025-09-01 12:05:05 +0530

    refactor: simplify routine check for read

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const checkRoutine = () => {
   const currentHour = new Date().getHours();
-  
-  if (currentHour >= 7 && currentHour < 8) {
+
+  if (currentHour === 7) {
     return "Read and study";
   }
 
